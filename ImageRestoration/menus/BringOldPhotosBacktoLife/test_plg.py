95a015ef797fcef6c30feee4e1cbca9fb814ab87
Xin Bo Qi <z003v5ns@code.siemens.com>
2021-10-12 10:10:53 +0800

    microsoft updated

--- ImageRestoration/menus/BringOldPhotosBacktoLife/test_plg.py
+++ ImageRestoration/menus/BringOldPhotosBacktoLife/test_plg.py
@@ -253,8 +253,6 @@ class RestoreImage(Simple):
             generated = model.inference(input, mask)
 
         output = ((generated.data.cpu()[0] + 1.0) / 2.0).permute(1, 2, 0).numpy()
-        print("Finish Stage 1 ...")
-        print("\n")
 
         self.app.show_img([output], ips.title+'-restored')
 
@@ -476,6 +474,9 @@ class RestoreScratchedImage(Simple):
 
 
         mask = self.app.get_img(ips.title+'-scratch').img
+
+        print("mask1.shape = ", mask.shape)
+
         if opt.mask_dilation != 0:
             kernel = np.ones((3,3),np.uint8)
             mask = cv2.dilate(mask,kernel,iterations = opt.mask_dilation)
@@ -492,10 +493,192 @@ class RestoreScratchedImage(Simple):
             generated = model.inference(input, mask)
 
         output = ((generated.data.cpu()[0] + 1.0) / 2.0).permute(1, 2, 0).numpy()
-        print("Finish Stage 1 ...")
-        print("\n")
 
-        self.app.show_img([output], ips.title+'-stage1')
+        self.app.show_img([output], ips.title+'-recovered')
 
 
-plgs = [RestoreImage, RestoreScratchedImage]
+class RestoreScratchedImageWithGivenScratch(Simple):
+    title = 'Restore Scratched Image with Given Scratch Mask'
+    note = ['rgb']
+
+    para = {'name': 'label2city',
+            'gpu_ids': '0',
+            'GPU': '0',
+            'checkpoints_dir': 'D:\\src\\weights\\oldphotostolife\\global_checkpoints\\checkpoints\\restoration\\',
+            'detection_checkpoints_dir': 'D:\\src\\weights\\oldphotostolife\\global_checkpoints\\checkpoints\\detection\\FT_Epoch_latest.pt',
+            # 'shape_predictor_dir': 'D:\\src\\weights\\oldphotostolife\\shape_predictor_68_face_landmarks.dat',
+            'isTrain': False,
+            'spatio_size': 64,
+            'model': 'pix2pixHD',
+            'norm': 'instance',
+            'use_dropout': False,
+            'data_type': 32,
+            'verbose': False,
+            'batchSize': 1,
+            'loadSize': 1024,
+            'fineSize': 512,
+            'label_nc': 35,
+            'input_nc': 3,
+            'output_nc': 3,
+            'resize_or_crop': 'scale_width',
+            'serial_batches': False,
+            'no_flip': False,
+            'nThreads': 2,
+            'max_dataset_size': float('inf'),
+            'netG': 'global',
+            'ngf': 64,
+            'k_size': 3,
+            'use_v2': False,
+            'mc': 1024,
+            'start_r': 3,
+            'n_downsample_global': 4,
+            'n_blocks_global': 9,
+            'n_blocks_local': 3,
+            'n_local_enhancers': 1,
+            'niter_fix_global': 0,
+            'load_pretrain': '',
+            'no_instance': False,
+            'instance_feat': False,
+            'label_feat': False,
+            'feat_num': 3,
+            'load_features': False,
+            'n_downsample_E': 4,
+            'nef': 16,
+            'n_clusters': 10,
+            'self_gen': False,
+            'map_mc': 64,
+            'kl': 0,
+            'load_pretrainA': '',
+            'load_pretrainB': '',
+            'feat_gan': False,
+            'no_cgan': False,
+            'map_unet': False,
+            'map_densenet': False,
+            'fcn': False,
+            'is_image': False,
+            'label_unpair': False,
+            'mapping_unpair': False,
+            'unpair_w': 1.0,
+            'pair_num': -1,
+            'Gan_w': 1,
+            'feat_dim': -1,
+            'abalation_vae_len': -1,
+            'use_skip_model': False,
+            'use_segmentation_model': False,
+            'test_random_crop': False,
+            'contain_scratch_L': False,
+            'mask_dilation': 0,
+            'irregular_mask': '',
+            'mapping_net_dilation': 1,
+            'non_local': '',
+            'NL_fusion_method': 'add',
+            'NL_use_mask': False,
+            'correlation_renormalize': False,
+            'Smooth_L1': False,
+            'face_restore_setting': 1,
+            'test_on_synthetic': False,
+            'use_SN': False,
+            'use_two_stage_mapping': False,
+            'L1_weight': 10,
+            'softmax_temperature': 1.0,
+            'patch_similarity': False,
+            'use_self': False,
+            'use_own_dataset': False,
+            'test_hole_two_folders': False,
+            'no_hole': False,
+            'random_hole': False,
+            'NL_res': False,
+            'image_L1': False,
+            'hole_image_no_mask': False,
+            'down_sample_degradation': False,
+            'norm_G': 'spectralinstance',
+            'init_G': 'xavier',
+            'use_new_G': False,
+            'use_new_D': False,
+            'cosin_similarity': False,
+            'downsample_mode': 'nearest',
+            'mapping_exp': 0,
+            'inference_optimize': False,
+            'ntest': float('inf'),
+            'aspect_ratio': 1.0,
+            'phase': 'test',
+            'no_degradation': False,
+            'no_load_VAE': False,
+            'use_v2_degradation': False,
+            'use_vae_which_epoch': 'latest',
+            'which_epoch': 'latest',
+            'multi_scale_test': 0.5,
+            'multi_scale_threshold': 0.5,
+            'scale_num': 1,
+            'test_mode': 'Full',
+            'Quality_restore': False,
+            'Scratch_and_Quality_restore': True,
+            'with_scratch': True,
+            'HR': False,
+            'checkpoint_name': 'Setting_9_epoch_100',
+            'input_size': 'full_size',
+            'scratch_img': ''
+           }
+    view = [(str, 'checkpoints_dir', 'Weights file at', 'directory'),
+            (str, 'detection_checkpoints_dir', 'Weights of detection at', 'directory'),
+            # (str, 'shape_predictor_dir', 'Weights of shape_predictor at', 'directory'),
+            (str, 'gpu_ids', 'gpu ids:', 'e.g. 0  0,1,2, 0,2. use -1 for CPU'),
+            # (list, 'input_nc', [1, 3], int, 'Input image', 'channels'),
+            # (list, 'output_nc', [1, 3], int, 'Output image', 'channels'),
+            (list, 'resize_or_crop', ['resize_and_crop', 'crop', 'scale_width', 'scale_width_and_crop'], str, 'scaling and cropping of images', 'when loading'),
+            ('img', 'scratch_img', 'Scratch image', ''),
+            (list, 'data_type', [8,16,32], int, 'Supported data type', 'bit')
+    ]
+
+
+    def run(self, ips, imgs, para = None):
+
+        opt = AttrDict(self.para)
+
+        str_ids = opt.gpu_ids.split(",")
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            int_id = int(str_id)
+            if int_id >= 0:
+                opt.gpu_ids.append(int_id)
+
+        parameter_set(opt)
+
+        ## 
+        model = Pix2PixHDModel_Mapping()
+
+        model.initialize(opt)
+        model.eval()
+
+        img_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        )
+        mask_transform = transforms.ToTensor()
+
+        input = ips.img
+        input_name = ips.name
+
+
+        mask = self.app.get_img(opt.scratch_img).img
+        mask = np.expand_dims(mask, axis=2)
+        print("mask2.shape = ", mask.shape)
+        if opt.mask_dilation != 0:
+            kernel = np.ones((3,3),np.uint8)
+            mask = cv2.dilate(mask,kernel,iterations = opt.mask_dilation)
+        origin = input
+
+        input = irregular_hole_synthesize(input, mask)
+        mask = mask_transform(mask)
+        mask = mask[:1, :, :]  ## Convert to single channel
+        mask = mask.unsqueeze(0)
+        input = img_transform(input)
+        input = input.unsqueeze(0)
+
+        with torch.no_grad():
+            generated = model.inference(input, mask)
+
+        output = ((generated.data.cpu()[0] + 1.0) / 2.0).permute(1, 2, 0).numpy()
+
+        self.app.show_img([output], ips.title+'-recovered')
+
+plgs = [RestoreImage, RestoreScratchedImage, RestoreScratchedImageWithGivenScratch]
